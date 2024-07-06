b1ef9a8914032b486d1087e673d443aec16cebac
Raise-NCCR <raise1229@gmail.com>
2024-07-06 22:14:29 +0900

    zsh.nix

--- zsh.nix
+++ zsh.nix
@@ -3,7 +3,7 @@
     enable = true;
     autocd = true; # cdなしでファイルパスだけで移動
     enableCompletion = true; # 自動補完
-    enableAutosuggestions = true; # 入力サジェスト
+    autosuggestions.enable = true; # 入力サジェスト
     syntaxHighlighting.enable = true; # シンタックスハイライト
     shellAliases = {
       myHome-flake = "home-manager switch --flake .#myHome";
