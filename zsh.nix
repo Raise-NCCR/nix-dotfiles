f45783e3edd1f6e882a68ebb570ba9537592d497
Raise-NCCR <raise1229@gmail.com>
2024-07-13 20:27:42 +0900

    bell

--- zsh.nix
+++ zsh.nix
@@ -2,7 +2,6 @@
   programs.zsh = {
     enable = true;
     autocd = true; # cdなしでファイルパスだけで移動
-    bell-style = "none";
     enableCompletion = true; # 自動補完
     autosuggestion.enable = true; # 入力サジェスト
     syntaxHighlighting.enable = true; # シンタックスハイライト
