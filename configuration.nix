f910b9d064dc7227d474e4ce2e811e1f603414b9
Raise-NCCR <raise1229@gmail.com>
2024-07-26 11:17:57 +0900

    nmap

--- configuration.nix
+++ configuration.nix
@@ -143,7 +143,7 @@
   services.openssh.passwordAuthentication = false;
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
+  networking.firewall.allowedTCPPorts = [ 22 ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
