465e4057ca1dfdd062756a77098c0b6612719d63
Raise-NCCR <raise1229@gmail.com>
2024-07-26 09:48:03 +0900

    ssh

--- configuration.nix
+++ configuration.nix
@@ -139,7 +139,8 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh.enable = true;
+  services.openssh.passwordAuthentication = false;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
